03020118b6db48c9fc590dccaba6fd34c0864433
Justin <aquino.j94@gmail.com>
2019-11-07 15:18:24 -0800

    Added Google Vision API Library

--- NodeTest.js
+++ NodeTest.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./testimage.jpg');
+  console.log("ANALYZING IMAGE");
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+
+
 app.listen(3000, () => {
+  quickstart();
   console.log('LISTENING ON PORT 3000');
 });
