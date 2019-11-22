a00794b18ba15467f510763a9b61e607f35a5e9e
Justin <aquino.j94@gmail.com>
2019-11-22 12:49:59 -0800

    Added google cloud storage integration, google cloud vision can retrieve image from storage to analyze

--- NodeTest.js
+++ NodeTest.js
@@ -1,8 +1,14 @@
+const {format} = require('util');
 const express = require('express');
 const multer = require('multer');
 
 const app = express();
-const upload = multer({dest: __dirname + '/images'});
+const upload = multer();
+
+const vision = require('@google-cloud/vision');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 
 /*app.use(express.static(path.join(__dirname, 'views')));*/
@@ -10,13 +16,32 @@ app.set('view engine', 'ejs');
 
 async function analyze(req, res) {
   // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
 
   // Creates a client
   const client = new vision.ImageAnnotatorClient();
 
   if ( req.file ) {
-      const [result] = await client.labelDetection('./images/' + req.file.filename);
+
+      const blob = bucket.file(req.file.originalname);
+      const blobStream = blob.createWriteStream({
+          resumable: false,
+      });
+
+      blobStream.on('error', err => {
+          next(err);
+      });
+
+      let publicUrl = '';
+      blobStream.on('finish', () => {
+          // The public URL can be used to directly access the file via HTTP.
+           publicUrl = format(
+              `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+          );
+      });
+
+      blobStream.end(req.file.buffer);
+
+      const [result] = await client.labelDetection('gs://nodevision.appspot.com/' + req.file.originalname);
       console.log("ANALYZING IMAGE");
       const labels = result.labelAnnotations;
       console.log('Labels:');
@@ -43,7 +68,7 @@ app.get('/HowTo', function(req, res) {
 app.post('/upload', upload.single('pic'), function(req, res) {
     let labels = null;
     if ( req.file ) {
-        analyze(req,res);
+        analyze(req, res).then(r => {});
         return;
     }
     res.render('ImageInsert', {
@@ -52,6 +77,7 @@ app.post('/upload', upload.single('pic'), function(req, res) {
 
 });
 
-app.listen(3000, () => {
-  console.log('LISTENING ON PORT 3000');
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT ${PORT}`);
 });
